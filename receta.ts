c406dd2086897a0e678b42c5d66e4784db2eeb80
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-04-27 23:39:46 -0300

    Initial commit

---
+++ receta.ts
@@ -0,0 +1,10 @@
+console.log("Receta de una Torta");
+console.log(
+  "Batir el aceite con el azúcar, agregar los huevos y mientras batimos incorporar la ralladura, leche y harina"
+);
+console.log("Enmantecar y enharinar un molde ");
+console.log(
+  "Colocar la preparación en el molde y espolvorear la superficie con azúcar"
+);
+console.log("Llevar a horno precalentado a 180º unos 35 minutos");
+console.log("retirar del horno, dejar enfriar unos minutos y desmoldar");
